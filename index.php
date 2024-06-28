66870e84136b9e3d9e05e040cac41608e6d431ae
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-06-28 13:27:01 -0300

    COMEÇO

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Museu</title>
+</head>
+<body>
+    <form action="index.php" method="POST"></form>
+    <fieldset>
+        <legend> <h1> Museu </h1> </legend>
+    </fieldset>
+    
+</body>
+</html>
