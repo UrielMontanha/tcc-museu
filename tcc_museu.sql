c7a2cd9ea91423454e02d4e3c7a497164bd5ca59
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-07-04 11:40:32 -0300

    Banco de dados

---
+++ tcc_museu.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 04-Jul-2024 às 14:39
+-- Versão do servidor: 8.0.31
+-- versão do PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tcc_museu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `avaliacoes`
+--
+
+DROP TABLE IF EXISTS `avaliacoes`;
+CREATE TABLE IF NOT EXISTS `avaliacoes` (
+  `id_aval` int DEFAULT NULL,
+  `id_usuario` varchar(255) DEFAULT NULL,
+  `id_obj` varchar(255) DEFAULT NULL,
+  `avaliacao` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comentarios`
+--
+
+DROP TABLE IF EXISTS `comentarios`;
+CREATE TABLE IF NOT EXISTS `comentarios` (
+  `id_com` int DEFAULT NULL,
+  `comentario` varchar(255) DEFAULT NULL,
+  `id_obj` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `objeto`
+--
+
+DROP TABLE IF EXISTS `objeto`;
+CREATE TABLE IF NOT EXISTS `objeto` (
+  `id_obj` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `data` datetime NOT NULL,
+  `condicao` varchar(255) NOT NULL,
+  `historia` text NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `pais_origem` varchar(255) NOT NULL,
+  `cidade_origem` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_obj`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `data_nasc` datetime NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `status` int NOT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `visitas`
+--
+
+DROP TABLE IF EXISTS `visitas`;
+CREATE TABLE IF NOT EXISTS `visitas` (
+  `id_visita` int DEFAULT NULL,
+  `data` datetime DEFAULT NULL,
+  `hora` datetime DEFAULT NULL,
+  `num_visitantes` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
