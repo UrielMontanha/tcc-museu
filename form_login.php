6ecab2b338f0824a67d257df4643586aff568fb2
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-07-06 20:58:40 -0300

    .

--- form_login.php
+++ form_login.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Museu</title>
 </head>
+
 <body>
     <form action="index.php" method="POST">
-    <fieldset>
-        <legend> <h1> Museu </h1> </legend>
-    </fieldset>
-    
+        <fieldset>
+            <legend>
+                <h1> Museu </h1>
+            </legend>
+        </fieldset>
+
     </form>
 </body>
+
 </html>
