65144023892073c9078bda90da1ccf3c3f3f8758
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-07-08 15:38:30 -0300

    coloquei o framework

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Museu</title>
 </head>
+
+<LINk rel="stylesheet" href="bootstrap.css">
 <body>
     <form action="index.php" method="POST">
-    <fieldset>
-        <legend> <h1> Museu </h1> </legend>
-    </fieldset>
-    
+        <fieldset>
+            <legend>
+                <h1> Museu </h1>
+            </legend>
+        </fieldset>
+        <div class="container-fluid">
+            ererer
+        </div>
     </form>
 </body>
+
 </html>
