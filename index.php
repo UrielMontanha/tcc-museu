b90b2c8cd8e92b50387aa1641ba7c4e7df0c6618
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-07-09 09:40:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> Login </title>
+    <link rel="stylesheet" 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
+    crossorigin="anonymous">
 </head>
 <body>
+<div class="container">
+  <h1> Login </h1>
+</div>
+    
     
 </body>
 </html>
