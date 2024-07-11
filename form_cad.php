164e7420284fb312e1658602e6fd86dc2caf757e
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-07-11 17:02:24 -0300

    Tentando começar

---
+++ form_cad.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+    <?php include 'links.php'?>
+</head>
+<body>
+
+<div class="container">
+  <h1>Cadastrar</h1>
+</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Login </title>
-    <link rel="stylesheet" 
-    href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
-    crossorigin="anonymous">
+    <title> Página Inicial </title>
+    <?php include 'links.php'?>
 </head>
 <body>
-<div class="container">
-  <h1> Login </h1>
+
+<div class="row">
+  <div class="col-9"> <h1> Museu Municipal </h1> </div>
+  <div class="col-4"><h1>.col-4<br>Já que 9 + 4 é maior que 12, esta div de quatro colunas pula para essa nova linha.</h1></div>
+  
+  <div class="col-6"><h1>.col-6<br>Colunas subsequentes pulam junto para a nova linha.</h1> </div>
 </div>
-    
     
 </body>
 </html>
