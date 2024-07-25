a3c24589399a721d10b284376b05d8c55a57385b
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-07-25 15:40:06 -0300

    update

--- index.php
+++ index.php
@@ -1,34 +1,41 @@
+<? session_start(); ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<?session_start();?>
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title> Página Inicial </title>
-  <?php include ("links.php"); ?>
+  <?php include("links.php"); ?>
 </head>
 
 <body>
 
   <div class="container-fluid">
+
     <h1>Museu Municipal</h1>
+
+    <br> <br>
+
+    <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link" href="form_cad.php">Cadastrar-se</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="form_login.php">Logar-se</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+    </ul>
+
   </div>
 
-<!--  <div class="row"> 
-  <div class="col-9">
-    <h1> Museu Municipal </h1>
-  </div>
-  <div class="col-4">
-    <h1>.col-4<br>Já que 9 + 4 é maior que 12, esta div de quatro colunas pula para essa nova linha.</h1>
-  </div>
-
-  <div class="col-6">
-    <h1>.col-6<br>Colunas subsequentes pulam junto para a nova linha.</h1>
-  </div> -->
-<?php
+  <!--<//?php
 echo $_SESSION['usuario'] . "ss";
-  echo "aa";
-?>
+?>-->
+
 </body>
 
 </html>
