f0cdae8ec36d0435c19d66fb7679dc3e0d1cccea
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-10-24 13:32:19 -0300

    ajustando uploads de arquivos

--- adm_form_cad_obj.php
+++ adm_form_cad_obj.php
@@ -25,7 +25,9 @@ session_start();
 
             Nome do objeto: <input type="text" name="nome" placeholder="Nome do objeto">
             <br> <br>
-            Data de criação: <input type="date" name="data_criacao" placeholder="Data de criação">
+            Data de criação: <input type="text" name="data_criacao" placeholder="Data de criação">
+            <br> <br>
+            Data de chegada: <input type="text" name="data_chegada" placeholder="Data de chegada">
             <br> <br>
             Condição: <input type="text" name="condicao" placeholder="Condição do objeto">
             <br> <br>
--- cad_obj.php
+++ cad_obj.php
@@ -5,14 +5,16 @@ require_once "conecta.php";
 $conexao = conectar();
 
 $nome = $_POST['nome'];
-$data = $_POST['data_criacao'];
+$data_cri = $_POST['data_criacao'];
+$data_che = $_POST['data_chegada'];
 $condicao = $_POST['condicao'];
 $pais_origem = $_POST['pais_origem'];
 $historia = $_POST['historia'];
 $foto = $_POST['foto'];
 
 
-$sql = "INSERT INTO objeto (nome, data_criacao, condicao, pais_origem, historia, foto) VALUES ('$nome', $data, '$condicao', '$pais_origem', '$historia' ,'$foto')";
+
+$sql = "INSERT INTO objeto (nome, data_criacao, data_chegada, condicao, pais_origem, historia, foto) VALUES ('$nome', $data_cri, $data_che, '$condicao', '$pais_origem', '$historia' ,'$foto')";
 
 $resultado = mysqli_query($conexao, $sql);
 
@@ -30,3 +32,68 @@ if ($resultado === true) {
     <a href='adm_form_cad_obj.php'>Continuar cadastrando</a>
     <br> <br> <a href='adm_form_museu.php'>Voltar</a><h3>";
 }
+
+<?php
+//////////////////////////////////////////////////////////////////////////////////////// $pastaDestino = "/uploads/";
+
+// verificar se o tamanho do arquivo é maior que 2 MB
+if ($_FILES['arquivo']['size'] > 2000000) {  // condição de guarda 👮
+    echo "O tamanho do arquivo é maior que o limite permitido. Limite máximo: 2 MB.";
+    die();
+}
+
+// verificar se o arquivo é uma imagem
+$extensao = strtolower(pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION));
+
+if (
+    $extensao != "png" && $extensao != "jpg" &&
+    $extensao != "jpeg" && $extensao != "gif" &&
+    $extensao != "jfif" && $extensao != "svg"
+) { // condição de guarda 👮
+    echo "O arquivo não é uma imagem! Apenas selecione arquivos 
+    com extensão png, jpg, jpeg, gif, jfif ou svg.";
+    die();
+}
+
+// verificar se é uma imagem de fato
+if (getimagesize($_FILES['arquivo']['tmp_name']) === false) {
+    echo "Problemas ao enviar a imagem. Tente novamente.";
+    die();
+}
+
+$nomeArquivo = uniqid();
+
+// se deu tudo certo até aqui, faz o upload
+$fezUpload = move_uploaded_file(
+    $_FILES['arquivo']['tmp_name'],
+    __DIR__ . $pastaDestino . $nomeArquivo . "." . $extensao
+);
+if ($fezUpload == true) {
+    $conexao = mysqli_connect("localhost", "root", "", "upload-arquivos");
+    $sql =/////////////////////////////////////////////////////////////////////// "INSERT INTO arquivo (nome_arquivo) VALUES ('$nomeArquivo.$extensao')";
+    $resultado = mysqli_query($conexao, $sql);
+    if ($resultado != false) {
+        // se for uma alteração de arquivo
+        if (isset($_POST['nome_arquivo'])) {
+            $apagou = unlink(__DIR__ . $pastaDestino . $_POST['nome_arquivo']);
+            if ($apagou == true) {
+                ////////////////////////////////////////////////////////////////// $sql = "DELETE FROM arquivo WHERE nome_arquivo='" 
+                //////////////////////////////////////////////////////////////////        . $_POST['nome_arquivo'] . "'";
+                $resultado2 = mysqli_query($conexao, $sql);
+                if ($resultado2 == false) {
+                    echo "Erro ao apagar o arquivo do banco de dados.";
+                    die();
+                }
+            } else {
+                echo "Erro ao apagar o arquivo antigo.";
+                die();
+            }
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////// header("Location: index.php");
+    } else {
+        echo "Erro ao registrar o arquivo no banco de dados.";
+    }
+} else {
+    echo "Erro ao mover arquivo.";
+}
+
