786c9ba3eb5397735227b0dae0d12dc0b5fd65f3
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-10-31 11:22:51 -0300

    .

--- banco_de dados/tcc_museu.sql
+++ banco_de dados/tcc_museu.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 16-Out-2024 às 21:59
+-- Tempo de geração: 31/10/2024 às 14:22
 -- Versão do servidor: 8.3.0
--- versão do PHP: 8.2.18
+-- Versão do PHP: 8.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `avaliacoes`
+-- Estrutura para tabela `avaliacoes`
 --
 
 DROP TABLE IF EXISTS `avaliacoes`;
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `avaliacoes` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `comentarios`
+-- Estrutura para tabela `comentarios`
 --
 
 DROP TABLE IF EXISTS `comentarios`;
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `comentarios` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `objeto`
+-- Estrutura para tabela `objeto`
 --
 
 DROP TABLE IF EXISTS `objeto`;
@@ -66,27 +66,26 @@ CREATE TABLE IF NOT EXISTS `objeto` (
   `id_obj` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `data_criacao` date NOT NULL,
+  `data_chegada` date NOT NULL,
   `condicao` varchar(255) NOT NULL,
   `historia` text NOT NULL,
-  `foto` varchar(255) NOT NULL,
+  `arquivo` date NOT NULL,
   `pais_origem` varchar(255) NOT NULL,
   `cidade_origem` varchar(255) NOT NULL,
   PRIMARY KEY (`id_obj`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `objeto`
+-- Despejando dados para a tabela `objeto`
 --
 
-INSERT INTO `objeto` (`id_obj`, `nome`, `data_criacao`, `condicao`, `historia`, `foto`, `pais_origem`, `cidade_origem`) VALUES
-(1, 'homem aranha', '0000-00-00', 'quase boa', 'É um estudante do ensino médio órfão que vive com seu tio Ben e sua tia May. Parker foi mordido por uma aranha radioativa em uma exposição científica e adquire a agilidade de um aracnídeo. O estudante é capaz de andar nas paredes e tetos e desenvolve um aparelho que o permite lançar teias artificiais.', '66b5f9ac4ec33.jpg', 'EUA', ''),
-(2, 'Espada medieval', '0000-00-00', 'Mais ou menos', 'A espada era o instrumento bélico favorito na Idade Média (seguido pelo arco e pela lança). É uma arma de curto alcance e, pelos conceitos da época, bem perigosa. A espada era utilizada em grande escala nessa época, nas Guerras Santas, nas batalhas contra os mouros etc.', 'espada-viking-ulfberht.webp', 'Inglaterra', ''),
-(3, 'Machado viking', '0000-00-00', 'Boa', 'Vikings e europeus medievais usavam machados como armas e ferramentas. Os machados vikings eram tipicamente menores e mais versáteis do que seus equivalentes medievais, que eram maiores e usados principalmente para a guerra.', 'mach.jpg', 'Noruega', '');
+INSERT INTO `objeto` (`id_obj`, `nome`, `data_criacao`, `data_chegada`, `condicao`, `historia`, `arquivo`, `pais_origem`, `cidade_origem`) VALUES
+(4, 'Spider', '0000-00-00', '0000-00-00', 'Boa', 'sssssss', '0000-00-00', 'EUA', '');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Estrutura para tabela `usuario`
 --
 
 DROP TABLE IF EXISTS `usuario`;
@@ -103,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 ) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `usuario`
+-- Despejando dados para a tabela `usuario`
 --
 
 INSERT INTO `usuario` (`id_usuario`, `nome`, `senha`, `data_nasc`, `cpf`, `status`, `email`) VALUES
@@ -116,7 +115,7 @@ INSERT INTO `usuario` (`id_usuario`, `nome`, `senha`, `data_nasc`, `cpf`, `statu
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `visita`
+-- Estrutura para tabela `visita`
 --
 
 DROP TABLE IF EXISTS `visita`;
@@ -129,18 +128,18 @@ CREATE TABLE IF NOT EXISTS `visita` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Restrições para despejos de tabelas
+-- Restrições para tabelas despejadas
 --
 
 --
--- Limitadores para a tabela `avaliacoes`
+-- Restrições para tabelas `avaliacoes`
 --
 ALTER TABLE `avaliacoes`
   ADD CONSTRAINT `fk_id_obj` FOREIGN KEY (`id_obj`) REFERENCES `objeto` (`id_obj`),
   ADD CONSTRAINT `fk_id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
 
 --
--- Limitadores para a tabela `comentarios`
+-- Restrições para tabelas `comentarios`
 --
 ALTER TABLE `comentarios`
   ADD CONSTRAINT `fk_c_id_obj` FOREIGN KEY (`id_obj`) REFERENCES `objeto` (`id_obj`),
