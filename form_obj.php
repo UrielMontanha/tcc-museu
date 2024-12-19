d4659ae7211041b94f07a15ba0dc9465c0102234
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-12-19 11:26:11 -0300

    Summary (required)

--- form_obj.php
+++ form_obj.php
@@ -30,7 +30,17 @@ $id_obj = $_GET['id_obj'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <style>
+        .row {
+            margin-top: 50px;
+        }
 
+        main.container img {
+            width: 100%;
+            height: auto;
+            /* Mantém a proporção da imagem */
+            display: block;
+            /* Remove espaçamento indesejado */
+        }
     </style>
 
 </head>
@@ -42,7 +52,46 @@ $id_obj = $_GET['id_obj'];
     <main class="container">
 
         <input type="hidden" name="id_obj" value="<?php echo $objeto['id_obj'] ?>">
+        <div class="row">
 
+            <img class="" src="css/imagens_obj/<?php echo $objeto['arquivo']; ?>">
+
+            <div class="col s12">
+                <h1 name="nome"> <?php echo $objeto['nome'] ?> </h1>
+            </div>
+
+
+            <div class="col s12">
+                <p class="grey-text text-darken-3 lighten-3" name="data_chegada"> Data de quando o objeto chegou no museu: <?php echo $objeto['data_chegada'] ?>. </p>
+
+                <p class="grey-text text-darken-3 lighten-3" name="data_criacao"> Data de quando o objeto foi criado: <?php echo $objeto['data_criacao'] ?>. </p>
+
+                <p class="grey-text text-darken-3 lighten-3" name="condicao"> Condição do objeto: <?php echo $objeto['condicao'] ?>. </p>
+
+                <p class="grey-text text-darken-3 lighten-3" name="pais_origem"> País origem do objeto: <?php echo $objeto['pais_origem'] ?>. </p>
+
+                <p class="grey-text text-darken-3 lighten-3" name="cidade_origem"> Cidade origem: <?php echo $objeto['cidade_origem'] ?>. </p>
+                <br>
+                <p class="grey-text text-darken-3 lighten-3"> História do objeto: </p>
+
+                <h6 class="black-text text-darken-3 lighten-3" name="historia"> <?php echo $objeto['historia'] ?> </h6>
+            </div>
+
+
+
+            <div class="col s12">
+                <div class="comment-box">
+
+                    <h3>Área de comentários</h3>
+
+                    <form action="#">
+                        <textarea name="coment" placeholder="Comente aqui..."></textarea>
+                        <button type="submit">Enviar comentário</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
     </main>
 
 </body>
--- header.php
+++ header.php
@@ -6,60 +6,10 @@ $pagina_Corrente = basename($_SERVER['SCRIPT_NAME']);
     <div class="navbar-fixed">
         <nav style="height: 100px;" class="grey darken-4">
             <div style="padding-top: 20px; padding-left: 0px; padding-right: 0px;" class="nav-wrapper container">
-                <a href="#" class="brand-logo" height="50"  width="10"> MUSEU MUNICIPAL </a>
+                <a href="#" class="brand-logo" height="50" width="10"> MUSEU MUNICIPAL </a>
 
                 <!-- Navbar items -->
                 <ul class="right hide-on-med-and-down">
-                    <li class="<?php if ($pagina_Corrente == 'form_login.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="form_login.php">Login</a>
-                    </li>
-                    <li class="<?php if ($pagina_Corrente == 'form_cad.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="form_cad.php">Cadastrar-se</a>
-                    </li>
-                    <li style="margin-left: 80px;" class="<?php if ($pagina_Corrente == 'index.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="index.php">Início</a>
-                    </li>
-                    <li class="<?php if ($pagina_Corrente == 'form_museu.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="form_museu.php">Visitar</a>
-                    </li>
-                    <li class="<?php if ($pagina_Corrente == 'contatos.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="contatos.php">Contatos</a>
-                    </li>
-                    <li><a href="sair.php">Sair</a></li>
-
-                    <li style="margin-left: 80px;"> <!-- Como fazer para colocar o "Usuário" bem para a direita. -->
-                        <h5>
-                            <?php if (isset($_SESSION['usuario'])) {
-                                echo "OLá " . $_SESSION['usuario'];
-                            } else {
-                                echo "Usuário anônimo"; 
-                            } ?>
-                        </h5>
-                    </li>
-                </ul>
-
-                <!-- Mobile Navbar -->
-                <ul id="nav-mobile" class="sidenav">
-                    <li class="<?php if ($pagina_Corrente == 'form_login.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="form_login.php">Login</a>
-                    </li>
-                    <li class="<?php if ($pagina_Corrente == 'form_cad.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="form_cad.php">Cadastrar-se</a>
-                    </li>
                     <li class="<?php if ($pagina_Corrente == 'index.php') {
                                     echo 'active';
                                 } ?>">
@@ -74,6 +24,58 @@ $pagina_Corrente = basename($_SERVER['SCRIPT_NAME']);
                                     echo 'active';
                                 } ?>">
                         <a href="contatos.php">Contatos</a>
+                    </li>
+
+                    <li style="margin-left: 80px;" class="<?php if ($pagina_Corrente == 'form_login.php') {
+                                                                echo 'active';
+                                                            } ?>">
+                        <a href="form_login.php">Login</a>
+                    </li>
+                    <li class="<?php if ($pagina_Corrente == 'form_cad.php') {
+                                    echo 'active';
+                                } ?>">
+                        <a href="form_cad.php">Cadastrar-se</a>
+                    </li>
+                    <li><a href="sair.php">Sair</a></li>
+
+                    <li style="margin-left: 80px;"> <!-- Como fazer para colocar o "Usuário" bem para a direita. -->
+                        <h5>
+                            <?php if (isset($_SESSION['usuario'])) {
+                                echo "OLá " . $_SESSION['usuario'];
+                            } else {
+                                echo "Usuário anônimo";
+                            } ?>
+                        </h5>
+                    </li>
+                </ul>
+
+                <!-- Mobile Navbar -->
+                <ul id="nav-mobile" class="sidenav">
+                    <li class="<?php if ($pagina_Corrente == 'index.php') {
+                                    echo 'active';
+                                } ?>">
+                        <a href="index.php">Início</a>
+                    </li>
+                    <li class="<?php if ($pagina_Corrente == 'form_museu.php') {
+                                    echo 'active';
+                                } ?>">
+                        <a href="form_museu.php">Visitar</a>
+                    </li>
+                    <li class="<?php if ($pagina_Corrente == 'contatos.php') {
+                                    echo 'active';
+                                } ?>">
+                        <a href="contatos.php">Contatos</a>
+                    </li>
+
+                    <li class="<?php if ($pagina_Corrente == 'form_login.php') {
+                                    echo 'active';
+                                } ?>">
+                        <a href="form_login.php">Login</a>
+                    </li>
+                    <li class="<?php if ($pagina_Corrente == 'form_cad.php') {
+                                    echo 'active';
+                                } ?>">
+                        <a href="form_cad.php">Cadastrar-se</a>
                     </li>
                     <li style="margin-left: 80px;">
                         <h5>
