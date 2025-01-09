602f8f8cbc5e1fc8f3d40386fa64dcd3765a3e6f
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2025-01-09 17:13:04 -0300

    ...

--- banco_de dados/tcc_museu.sql
+++ banco_de dados/tcc_museu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 28/11/2024 às 18:21
+-- Tempo de geração: 09/01/2025 às 20:12
 -- Versão do servidor: 8.3.0
 -- Versão do PHP: 8.2.18
 
@@ -73,16 +73,37 @@ CREATE TABLE IF NOT EXISTS `objeto` (
   `pais_origem` varchar(255) NOT NULL,
   `cidade_origem` varchar(255) NOT NULL,
   PRIMARY KEY (`id_obj`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Despejando dados para a tabela `objeto`
 --
 
 INSERT INTO `objeto` (`id_obj`, `nome`, `data_criacao`, `data_chegada`, `condicao`, `historia`, `arquivo`, `pais_origem`, `cidade_origem`) VALUES
-(4, 'Spider-man', '0000-00-00', '0000-00-00', 'Boa', 'sssssss', '67489752ae4f6', 'EUA', ''),
-(7, 'sss', '0000-00-00', '0000-00-00', 'sss', 'sss', '67365634538eb.jpg', 'sss', ''),
-(9, 'ssss', '2013-08-08', '2024-10-30', 'sss', 'sss', '674889b5b39b0.jpg', 'sss', '');
+(12, 'Espada medieval', '1500-02-04', '1980-05-08', 'Boa', 'What is Lorem Ipsum?\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n\r\nWhy do we use it?\r\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n\r\n\r\nWhere does it come from?\r\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.', '67782b3183bc5.jpg', 'Portugal', ''),
+(13, ' TRIUMPH 1800 ROADSTER', '1946-01-01', '2004-03-04', 'Média', 'Why do we use it?\r\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n\r\n\r\nWhere does it come from?\r\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.', '67782d3338250.jpg', 'EUA', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `recuperar_senha`
+--
+
+DROP TABLE IF EXISTS `recuperar_senha`;
+CREATE TABLE IF NOT EXISTS `recuperar_senha` (
+  `email` varchar(255) NOT NULL,
+  `token` char(100) NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `usado` tinyint NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Despejando dados para a tabela `recuperar_senha`
+--
+
+INSERT INTO `recuperar_senha` (`email`, `token`, `data_criacao`, `usado`) VALUES
+('uriel.2022316043@aluno.iffar.edu.br', '4c06af688027deceb8918ecd4e121217f7fb0199039c8f81d9555ed8d7dcd8bf06e99305dac40e060f6b59c29a83dc7af281', '2025-01-06 11:20:32', 0),
+('uriel.2022316043@aluno.iffar.edu.br', '0eb3d9bd5f38d76302fa8e91eead965283f059335ad95c31aac07db24d573b33e67ee20309375b7b41acb6639d4b8fa3c794', '2025-01-06 11:32:54', 1);
 
 -- --------------------------------------------------------
 
@@ -98,19 +119,20 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `data_nasc` date NOT NULL,
   `cpf` varchar(11) NOT NULL,
   `status` int NOT NULL,
+  `foto_perfil` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`id_usuario`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Despejando dados para a tabela `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `nome`, `senha`, `data_nasc`, `cpf`, `status`, `email`) VALUES
-(34, 'Mundo Animal', 'ddd', '2024-07-27', '00022233352', 0, 'mundoanimal@gmail.com'),
-(37, 'Uriel', '123', '2000-05-24', '77788899900', 1, 'uriel@aluno'),
-(38, 'Jonas', '22', '2000-05-24', '44433322211', 0, 'jonas.2022310916@aluno.iffar.edu.br');
+INSERT INTO `usuario` (`id_usuario`, `nome`, `senha`, `data_nasc`, `cpf`, `status`, `foto_perfil`, `email`) VALUES
+(34, 'Mundo Animal', 'ddd', '2024-07-27', '00022233352', 0, '', 'mundoanimal@gmail.com'),
+(37, 'admnistrador', '123', '2000-05-24', '77788899900', 1, '', 'adm@gmail.com'),
+(39, 'Uriel', '$argon2i$v=19$m=65536,t=4,p=1$QS5PS2V1cDhnZC5HR3ZwUw$Lfc0dXNOIcc+p8JXvwC1e96DUTfDHaOWq9nTZ6SuAmk', '0000-00-00', '05767293023', 0, '', 'uriel.2022316043@aluno.iffar.edu.br');
 
 -- --------------------------------------------------------
 
--- header.php
+++ header.php
@@ -1,98 +1,103 @@
 <?php
+
 $pagina_Corrente = basename($_SERVER['SCRIPT_NAME']);
+
+if (!(isset($_SESSION["usuario"]))) {
+
+?>
+    <header>
+        <div class="navbar-fixed">
+            <nav style="height: 100px;" class="grey darken-4">
+                <div style="padding-top: 20px; padding-left: 120px; padding-right: 0px;" class="nav-wrapper">
+
+                    <a href="#" class="brand-logo">MUSEU MUNICIPAL</a>
+
+                    <ul class="right hide-on-med-and-down">
+                        <li class="<?php if ($pagina_Corrente == 'index.php') {
+                                        echo 'active';
+                                    } ?>">
+                            <a href="index.php">Início</a>
+                        </li>
+                        <li style="margin-right: 300px;" class="<?php if ($pagina_Corrente == 'form_museu.php') {
+                                                                    echo 'active';
+                                                                } ?>">
+                            <a href="form_museu.php">Visitar</a>
+                        </li>
+
+                        <li style="margin-right: 200px;"> <!-- Como fazer para colocar o "Usuário" bem para a direita. -->
+                            <h5>
+                                <?php if (isset($_SESSION['usuario'])) {
+                                    echo "OLá " . $_SESSION['usuario'];
+                                } else {
+                                    echo "Usuário anônimo";
+                                } ?>
+                            </h5>
+                        </li>
+
+                        <!-- <div class="input-field">
+            <input id="search" type="search" required>
+            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+            <i class="material-icons">close</i>
+        </div>                                               O que há de errado?                  -->
+
+                        <li style="margin-left: 80px;" class="<?php if ($pagina_Corrente == 'form_login.php') {
+                                                                    echo 'active';
+                                                                } ?>">
+                            <a href="form_login.php">Login</a>
+                        </li>
+
+                        <li><a href="sair.php">Sair</a></li>
+                    </ul>
+
+
+
+
+
+
+
+                    <!-- Mobile Navbar -->
+                    <ul id="nav-mobile" class="sidenav">
+                        <li class="<?php if ($pagina_Corrente == 'index.php') {
+                                        echo 'active';
+                                    } ?>">
+                            <a href="index.php">Início</a>
+                        </li>
+                        <li class="<?php if ($pagina_Corrente == 'form_museu.php') {
+                                        echo 'active';
+                                    } ?>">
+                            <a href="form_museu.php">Visitar</a>
+                        </li>
+
+                        <li class="<?php if ($pagina_Corrente == 'form_login.php') {
+                                        echo 'active';
+                                    } ?>">
+                            <a href="form_login.php">Login</a>
+                        </li>
+                        <li style="margin-left: 80px;">
+                            <h5>
+                                <?php if (isset($_SESSION['usuario'])) {
+                                    echo "OLá " . $_SESSION['usuario'];
+                                } else {
+                                    echo "Usuário anônimo";
+                                } ?>
+                            </h5>
+                        </li>
+                    </ul>
+                    <!-- Button for mobile menu -->
+                    <a href="menu.php" data-target="nav-mobile" class="sidenav-trigger">
+                        <i class="material-icons">menu</i>
+                    </a>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+<?php
+} else {session_start();
 ?>
 
-<header>
-    <div class="navbar-fixed">
-        <nav style="height: 100px;" class="grey darken-4">
-            <div style="padding-top: 20px; padding-left: 0px; padding-right: 0px;" class="nav-wrapper container">
-            <a href="#" class="brand-logo">MUSEU MUNICIPAL</a>
-                <!-- <a href="#" class="brand-logo" height="50" width="10"> MUSEU MUNICIPAL </a> -->
 
-                <!-- Navbar items -->
-                <ul class="right hide-on-med-and-down">
-                    <li class="<?php if ($pagina_Corrente == 'index.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="index.php">Início</a>
-                    </li>
-                    <li class="<?php if ($pagina_Corrente == 'form_museu.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="form_museu.php">Visitar</a>
-                    </li>
-                    <li class="<?php if ($pagina_Corrente == 'contatos.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="contatos.php">Contatos</a>
-                    </li>
 
-                    <li style="margin-left: 80px;" class="<?php if ($pagina_Corrente == 'form_login.php') {
-                                                                echo 'active';
-                                                            } ?>">
-                        <a href="form_login.php">Login</a>
-                    </li>
-                    <li class="<?php if ($pagina_Corrente == 'form_cad.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="form_cad.php">Cadastrar-se</a>
-                    </li>
-                    <li><a href="sair.php">Sair</a></li>
-
-                    <li style="margin-left: 80px;"> <!-- Como fazer para colocar o "Usuário" bem para a direita. -->
-                        <h5>
-                            <?php if (isset($_SESSION['usuario'])) {
-                                echo "OLá " . $_SESSION['usuario'];
-                            } else {
-                                echo "Usuário anônimo";
-                            } ?>
-                        </h5>
-                    </li>
-                </ul>
-
-                <!-- Mobile Navbar -->
-                <ul id="nav-mobile" class="sidenav">
-                    <li class="<?php if ($pagina_Corrente == 'index.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="index.php">Início</a>
-                    </li>
-                    <li class="<?php if ($pagina_Corrente == 'form_museu.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="form_museu.php">Visitar</a>
-                    </li>
-                    <li class="<?php if ($pagina_Corrente == 'contatos.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="contatos.php">Contatos</a>
-                    </li>
-
-                    <li class="<?php if ($pagina_Corrente == 'form_login.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="form_login.php">Login</a>
-                    </li>
-                    <li class="<?php if ($pagina_Corrente == 'form_cad.php') {
-                                    echo 'active';
-                                } ?>">
-                        <a href="form_cad.php">Cadastrar-se</a>
-                    </li>
-                    <li style="margin-left: 80px;">
-                        <h5>
-                            <?php if (isset($_SESSION['usuario'])) {
-                                echo "OLá " . $_SESSION['usuario'];
-                            } else {
-                                echo "Usuário anônimo";
-                            } ?>
-                        </h5>
-                    </li>
-                </ul>
-                <!-- Button for mobile menu -->
-                <a href="#" data-target="nav-mobile" class="sidenav-trigger">
-                    <i class="material-icons">menu</i>
-                </a>
-            </div>
-        </nav>
-    </div>
-</header>
+<?php
+}
+?>
